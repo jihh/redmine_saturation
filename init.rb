b21edc84b27bf00a29f49a2f344f793481378751
jihh <jihh@neusoft.com>
2016-10-10 14:17:49 +0800

    First commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_saturation do
+  name 'Redmine Saturation plugin'
+  author 'Haihan Ji'
+  description '这个插件可以在我的工作台中增加一个查看每月工作量是否饱和的区块。'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
